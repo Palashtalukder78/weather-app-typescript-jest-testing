2ae872f162a648634fdc0c3b71a766801e145cd5
Md Ariful Islam Palash <Md Ariful Islam Palash>
2022-04-25 13:45:08 +0600

    Solve testing an error

--- src/_testing_/country.test.tsx
+++ src/_testing_/country.test.tsx
@@ -1,13 +1,16 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import Country from "../components/Country/Country"
-import FetchCountry from "../fetch/fetch"
+import {createMemoryHistory} from 'history';
+import { Router } from "react-router-dom";
 
-test('fetch weather in the component', async()=>{
-   render(<Country></Country>) 
-   const data = await FetchCountry.getWeather('bangladesh');
-   expect(data).toBeDefined();
-})
-test('fetch country in the documnet', async()=>{
-    const data = await FetchCountry.getCountry('bangladesh')
-    expect(data).toBeDefined();
+test('render country info',()=>{
+    const history = createMemoryHistory();
+    history.push('/country/country')
+
+    render(<Router history={history}>
+        <Country></Country>
+    </Router>)
+
+    const details = screen.getByTestId('country');
+    expect(details).toBeInTheDocument()
 })
